39fa915d008a12f933f5d38cd340b58bf1718180
edelstarkg@mail.ru <edelstarkg@mail.ru@38ae2757-8e90-eff6-40c7-9a11b2003ba3>
2010-05-31 10:21:55 +0000

    Начало работы по подключению языкового файла и переводу компонента на русский язык.

---
+++ trunk/administrator/components/com_jp/languages/russian.php
@@ -0,0 +1,38 @@
+<?php 
+define("JP_CONTROL_PANEL", "Контрольная панель JPromoter");
+define("JP_COUNTS", "Счетчики");
+define("JP_SUMMARY", "Краткий анализ");
+define("JP_SIMULATE_LINKS", "Имитированных ссылок");
+define("JP_SEF_URLS", "SEF ссылок");
+define("JP_OPTIMIZED_PAGES", "Оптимизированных страниц");
+define("JP_EXTERNAL_LINKS", "Внешних ссылок");
+define("JP_IMAGES", "Изображений");
+define("JP_SE_SIMULATION", "Эмулятор ПС");
+define("JP_SEF_URL_NAME", "&nbsp;&nbsp;SEF ссылки");
+define("JP_SIMULATION_DATE", "Дата эмуляции");
+define("JP_INDEXED_LINKS", "Проиндексированные ссылки");
+define("JP_SIMULATION_STATUS", "Статус эмуляции");
+define("JP_SIMULATION_OPTIONS", "Опции эмуляции");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+define("JP_", "");
+
+?>
